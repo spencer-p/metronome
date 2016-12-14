c4cc9c1a47e45a2eb38cc0243ddff2e0fd80f751
Spencer Peterson <spencerjpeterson@gmail.com>
2016-12-13 17:07:23 -0800

    move dial code to class

---
+++ dial.lua
@@ -0,0 +1,43 @@
+--
+-- dial.lua
+--
+
+Dial = Class{}
+
+function Dial:init(x, y, radius)
+	self.pos, self.radius = vector(x, y), radius
+	self.value = 0
+end
+
+function Dial:update(dt)
+	if self.value < 0 then self.value = 0 end
+end
+
+function Dial:draw()
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.circle("fill", self.pos.x, self.pos.y, self.radius)
+	love.graphics.setColor(0xff, 0xff, 0xff)
+	love.graphics.circle("fill", self.pos.x, self.pos.y, self.radius/3)
+end
+
+function Dial:mousepressed(x, y)
+	self.pressed = true
+end
+
+function Dial:mousereleased(x, y)
+	self.pressed = false
+end
+
+function Dial:mousemoved(x, y, dx, dy)
+	if self.pressed then
+		local oldtocenter = vector(x-dx, y-dy) - self.pos
+		local tocenter = vector(x, y) - self.pos
+		local changeinangle = tocenter:angleTo(oldtocenter)
+		if math.abs(changeinangle) > 6 then return end
+		if changeinangle < 0 then
+			self.value = self.value - math.abs(changeinangle*10)
+		else
+			self.value = self.value + math.abs(changeinangle*10)
+		end
+	end
+end
--- main.lua
+++ main.lua
@@ -2,51 +2,35 @@
 -- main.lua
 --
 
+Class = require "hump.class"
 vector = require "hump.vector"
+
+require "dial"
 
 function love.load()
 	love.graphics.setBackgroundColor(0xff, 0xff, 0xff)
-	dial = {
-		center = vector(love.graphics.getWidth()/2, love.graphics.getHeight()/2),
-		draw = function()
-			love.graphics.setColor(0, 0, 0)
-			love.graphics.circle("fill", dial.center.x, dial.center.y, 150)
-			love.graphics.setColor(0xff, 0xff, 0xff)
-			love.graphics.circle("fill", dial.center.x, dial.center.y, 30)
-		end
-	}
-	num = 0
+	tempo = Dial(love.graphics.getWidth()/2, love.graphics.getHeight()/2, love.graphics.getHeight()/4)
+	--time = Dial(love.graphics.getWidth()/5, 4*love.graphics.getHeight()/5, love.graphics.getHeight()/8)
 end
 
 function love.update(dt)
-	num = num - dt
-	if num < 0 then num = 0 end
+	tempo:update(dt)
 end
 
 function love.draw()
-	dial:draw()
+	tempo:draw()
 	love.graphics.setColor(0, 0, 0)
-	love.graphics.print(os.date("!%X", math.floor(num)))
+	love.graphics.print(os.date("!%X", math.floor(tempo.value)))
 end
 
 function love.mousepressed(x, y)
-	mousedown = true
+	tempo:mousepressed()
 end
 
 function love.mousereleased(x, y)
-	mousedown = false
+	tempo:mousereleased()
 end
 
 function love.mousemoved(x, y, dx, dy)
-	if mousedown then
-		local oldtocenter = vector(x-dx, y-dy) - dial.center
-		local tocenter = vector(x, y) - dial.center
-		local changeinangle = tocenter:angleTo(oldtocenter)
-		if math.abs(changeinangle) > 6 then return end
-		if changeinangle < 0 then
-			num = num - 0.25
-		else
-			num = num + 0.25
-		end
-	end
+	tempo:mousemoved(x, y, dx, dy)
 end
